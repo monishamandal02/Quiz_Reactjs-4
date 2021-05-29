f0932e826a4beda0c3eb94183408886790c89901
monishamandal02 <monishamandal0203@gmail.com>
2021-05-29 15:44:29 +0530

    Updates

---
+++ precache-manifest.10e20db9ee6dda359dd05a945379ab2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7764f0dae313a61a40e62a01455386ff",
+    "url": "/Quiz_Reactjs-4/index.html"
+  },
+  {
+    "revision": "48e53e0c0bf03e7289b5",
+    "url": "/Quiz_Reactjs-4/static/css/main.29da917b.chunk.css"
+  },
+  {
+    "revision": "83658f5aad0f164c9334",
+    "url": "/Quiz_Reactjs-4/static/js/2.199f503d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz_Reactjs-4/static/js/2.199f503d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e53e0c0bf03e7289b5",
+    "url": "/Quiz_Reactjs-4/static/js/main.0e469f03.chunk.js"
+  },
+  {
+    "revision": "6c791d57eca37b5f0d26",
+    "url": "/Quiz_Reactjs-4/static/js/runtime-main.1659c091.js"
+  },
+  {
+    "revision": "f68b524c22ee3f015b9ac004923e5dd1",
+    "url": "/Quiz_Reactjs-4/static/media/background.f68b524c.jpg"
+  }
+]);
